53bc88eaa6b287a584a0527e7a2a7e86097762e0
Basavaraj Bandi <98872884+Erajbandi@users.noreply.github.com>
2022-04-09 22:55:32 +0530

    Add files via upload
    SQL Activities

---
+++ SQL/Activities/Activity11.sql
@@ -0,0 +1,307 @@
+REM   Script: Activity11
+REM   Activity11
+
+describe salesman
+
+
+DESCRIBE salesman:
+
+
+DESCRIBE salesman
+
+
+SELECT * FROM salesman;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+SELECT customer_id, customer_name FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION 
+SELECT salesman_id, name FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT a.salesman_id, name, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, name, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+DESCRIBE salesman
+
+
+SELECT customer_id, customer_name FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION 
+SELECT salesman_id, ALESMAN_NAME FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT a.salesman_id, ALESMAN_NAME, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, ALESMAN_NAME, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+DESCRIBE salesman
+
+
+DESCRIBE customers
+
+
+DESCRIBE salesman
+
+
+E salesman
+
+
+SELECT customer_id, customer_name FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION 
+SELECT salesman_id, ALESMAN_NAME FROM salesman a 
+WHERDESCRIBE customersE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT a.salesman_id, ALESMAN_NAME, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, ALESMAN_NAME, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+SELECT customer_id, customer_name FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION 
+SELECT salesman_id, name FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT a.salesman_id, name, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, name, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+DESCRIBE customers
+
+
+CREATE DATABASE activities;
+
+CREATE TABLE Customers ( 
+    CustomerID int, 
+    CustomerName varchar(52), 
+    ContactName varchar(32), 
+    Address varchar(255), 
+    City varchar(50), 
+    PostalCode varchar(20), 
+    Country varchar(20) 
+);
+
+CREATE TABLE Customers ( 
+    CustomerID int, 
+    CustomerName varchar(52), 
+    ContactName varchar(32), 
+    Address varchar(255), 
+    City varchar(50), 
+    PostalCode varchar(20), 
+    Country varchar(20) 
+);
+
+DESCRIBE Customers
+
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) 
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+
+SELECT customer_id, customer_name FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION 
+SELECT salesman_id, name FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT a.salesman_id, name, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, name, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+dESCRIBE customers
+
+
+SELECT customer_id, CUSTOMERNAME FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION 
+SELECT salesman_id, name FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT a.salesman_id, name, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, name, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+dESCRIBE customers
+
+
+DESCRIBE orders
+
+
+create table orders( 
+    order_no int primary key, purchase_amount float, order_date date, 
+    customer_id int, salesman_id int);
+
+INSERT ALL 
+    INTO orders VALUES(70001, 150.5, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3005, 5002)  
+    INTO orders VALUES(70009, 270.65, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3001, 5005) 
+    INTO orders VALUES(70002, 65.26, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3002, 5001) 
+    INTO orders VALUES(70004, 110.5, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3009, 5003) 
+    INTO orders VALUES(70007, 948.5, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3005, 5002) 
+    INTO orders VALUES(70005, 2400.6, TO_DATE('2012/07/27', 'YYYY/MM/DD'), 3007, 5001) 
+    INTO orders VALUES(70008, 5760, TO_DATE('2012/08/15', 'YYYY/MM/DD'), 3002, 5001) 
+    INTO orders VALUES(70010, 1983.43, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3004, 5006) 
+    INTO orders VALUES(70003, 2480.4, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3009, 5003) 
+    INTO orders VALUES(70012, 250.45, TO_DATE('2012/06/27', 'YYYY/MM/DD'), 3008, 5002) 
+    INTO orders VALUES(70011, 75.29, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3003, 5007) 
+    INTO orders VALUES(70013, 3045.6, TO_DATE('2012/04/25', 'YYYY/MM/DD'), 3002, 5001) 
+SELECT 1 FROM DUAL;
+
+SELECT customer_id, CUSTOMERNAME FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION 
+SELECT salesman_id, name FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT a.salesman_id, name, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, name, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+DESCRIBE salesman
+
+
+DESCRIBE customers
+
+
+SELECT customer_id, CUSTOMERNAME FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION 
+SELECT salesman_id, name FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY CUSTOMERNAME;
+
+SELECT a.salesman_id, name, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, name, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+DESCRIBE customers
+
+
+DESCRIBE salesman
+
+
+SELECT customer_id, CUSTOMERNAME FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.CUSTOMERID = b.CUSTOMERID) 
+UNION 
+SELECT salesman_id, SALESMAN_NAME FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY CUSTOMERNAME;
+
+SELECT a.salesman_id, SALESMAN_NAME, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, SALESMAN_NAME, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+DESCRIBE customers
+
+
+SELECT CUSTOMERID, CUSTOMERNAME FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.CUSTOMERID = b.CUSTOMERID) 
+UNION 
+SELECT salesman_id, SALESMAN_NAME FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY CUSTOMERNAME;
+
+SELECT a.salesman_id, SALESMAN_NAME, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, SALESMAN_NAME, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+DESCRIBE orders
+
+
+SELECT CUSTOMERID, CUSTOMERNAME FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.CUSTOMER_ID = b.CUSTOMER_ID) 
+UNION 
+SELECT salesman_id, SALESMAN_NAME FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY CUSTOMERNAME;
+
+SELECT a.salesman_id, SALESMAN_NAME, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, SALESMAN_NAME, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+DESCRIBE orders 
+
+
+SELECT CUSTOMERID, CUSTOMERNAME FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.CUSTOMERID = b.CUSTOMER_ID) 
+UNION 
+SELECT salesman_id, SALESMAN_NAME FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY CUSTOMERNAME;
+
+SELECT a.salesman_id, SALESMAN_NAME, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, SALESMAN_NAME, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,80 @@
+REM   Script: Activity 4
+REM   Alter update and select statement
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+select AVG(purchase_amount) AS "Average" from orders;
+
+select MAX(purchase_amount) AS "Maximum" from orders;
+
+select MIN(purchase_amount) AS "Minumum" from orders;
+
+select COUNT(distinct salesman_id) AS "Total count" from orders;
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+select AVG(purchase_amount) AS "Average" from orders;
+
+select MAX(purchase_amount) AS "Maximum" from orders;
+
+select MIN(purchase_amount) AS "Minumum" from orders;
+
+select COUNT(distinct salesman_id) AS "Total count" from orders;
+
+CREATE TABLE tableName;
+
+CREATE TABLE salesman;
+
+CREATE TABLE salesman;
+
+CREATE TABLE salesman ( 
+	 
+    salesman_id int, 
+	 
+    salesman_name varchar2(32), 
+	 
+    salesman_city varchar2(32), 
+	 
+    commission int;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+SELECT salesman_id, salesman_city FROM salesman;
+
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
